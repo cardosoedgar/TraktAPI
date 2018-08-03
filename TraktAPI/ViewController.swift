1d39dfffcf9d7754c809ce31f615cf0f1cf43e16
Edgar Cardoso <edgar.costacardoso@gmail.com>
2018-08-02 21:51:15 -0300

    Creates project

---
+++ TraktAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TraktAPI
+//
+//  Created by Edgar Cardoso on 02/08/18.
+//  Copyright © 2018 Edgar Cardoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
